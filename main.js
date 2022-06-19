40874153851c87af906952e776622951df470e2d
PROCODER123456789 <99879654+PROCODER123456789@users.noreply.github.com>
2022-06-19 14:52:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke (0);
+
+}
